acc41f62c27d5ff967b40e3b41c0d8e4e02f7b66
DanLaZ <lazenberry_daniel@yahoo.com>
2019-06-30 15:49:10 -0400

    OMDb api is working correctly | Going to link the api call to liri.js

--- command.js
+++ command.js
@@ -6,16 +6,17 @@ var Command = function() {
 
     var divider = "\n------------------------------------------------------------\n\n";
 
-    this.findConcert = function() {
+    this.findConcert = function(artist) {
 
-        const url = "https://rest.bandsintown.com/artists/" + "Cardi B" + "/events?app_id=codingbootcamp";
+        const url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
         axios.get(url).then(function (response) {
             // handle success
             var concertInfo = response.data[0];
+            var venueInfo = concertInfo.venue;
         
-            var venueCity = concertInfo.venue.city;
-            var venueCountry = concertInfo.venue.country;
+            var venueCity = venueInfo.city;
+            var venueCountry = venueInfo.country;
             var eventDate = concertInfo.datetime;
 
             var concertData = [
@@ -26,14 +27,53 @@ var Command = function() {
 
             console.log(concertData);
         })
-        fs.appendFile("log.txt", concertData + divider, function(err) {
-          if (err) throw err;
-          console.log(concertData);
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+          // always executed
         });
+     
+      
         
     }
 };
 
+const URL = "http://www.omdbapi.com/?apikey=trilogy&t=" + "Blade";
 
+axios.get(URL).then(function (response) {
+    
+    var omdbInfo = response.data;
+
+    var omdbData = [
+        "Title: " + omdbInfo.Title,
+        "Year Released: " + omdbInfo.Year,
+        "IMDB Rating: " + omdbInfo.Ratings[0].Value,
+        "Rotten Tomatoes Rating: " + omdbInfo.Ratings[1].Value,
+        "Country Movie was Produced in: " + omdbInfo.Country,
+        "Language: " + omdbInfo.Language,
+        "Actors: " + omdbInfo.Actors 
+    ].join("\n");
+
+    console.log(omdbData);
+})
+.catch(function (error) {
+  // handle error
+  console.log(error);
+})
+.finally(function () {
+  // always executed
+});
+
+
+
+
+
+
+
+// fs.appendFile("log.txt", concertData + divider, function() {
+//     console.log(concertData);
+//   });
 
 module.exports = Command;
--- liri.js
+++ liri.js
@@ -1,9 +1,21 @@
 require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-
+// var keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
 var Command = require("./command.js");
 var command = new Command();
 
 var search = process.argv[2];
 var term = process.argv.slice(3).join(" ");
+
+if (search === "concert") {
+    console.log("Searching for concert info...");
+    command.findConcert(term);
+} 
+else if (search === "song") {
+    console.log("Searching for song info...");
+    command.findSong(term);
+}
+else {
+    console.log("Searching for movie info...");
+    command.findMovie(term);
+}

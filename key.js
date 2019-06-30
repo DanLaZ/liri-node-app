df216be90bea54c6e57ae112d6e6c375dba53018
DanLaZ <lazenberry_daniel@yahoo.com>
2019-06-30 12:13:34 -0400

    Getting a response from the band in town api | I'm grabbing the info I want and cleaned up the JSON

--- key.js
+++ key.js
@@ -4,3 +4,35 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+const url = "https://rest.bandsintown.com/artists/" + "Cardi B" + "/events?app_id=codingbootcamp";
+
+const axios = require("axios");
+axios.get(url)
+  .then(function (response) {
+    // handle success
+    var concertInfo = response.data[0];
+   
+    var venueCity = concertInfo.venue.city;
+    var venueCountry = concertInfo.venue.country;
+    var eventDate = concertInfo.datetime;
+    
+
+    var concertData = [
+      "Concert Name: " + concertInfo.venue.name,
+      "Location: " + venueCity + "," + venueCountry,
+      "Date: " + eventDate,
+    ].join("\n");
+
+    console.log(concertData);
+   
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+
+
